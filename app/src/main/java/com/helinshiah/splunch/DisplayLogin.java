567b02da4754ecd09624c495e574d273b802f981
helin24 <helin.shiah@gmail.com>
2015-03-22 10:06:43 -0700

    connection established

--- app/src/main/java/com/helinshiah/splunch/DisplayLogin.java
+++ app/src/main/java/com/helinshiah/splunch/DisplayLogin.java
@@ -1,24 +1,49 @@
 package com.helinshiah.splunch;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
 
 
 public class DisplayLogin extends ActionBarActivity {
+    private static final String DEBUG_TAG = "HttpExample";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText(message);
-        setContentView(textView);
+//        Intent intent = getIntent();
+//        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+//        TextView textView = new TextView(this);
+//        textView.setTextSize(40);
+//        textView.setText(message);
+//        loginLayout.addView(textView);
+//        setContentView(loginLayout);
+        setContentView(R.layout.activity_display_login);
     }
 
     @Override
@@ -34,5 +59,80 @@ public class DisplayLogin extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void map_sandwich(View view) {
+        Uri location = Uri.parse("geo:0,0?q=Bunk+Bar+Water+Portland");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+
+        PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> activities = packageManager.queryIntentActivities(mapIntent, 0);
+        boolean isIntentSafe = activities.size() > 0;
+
+        if (isIntentSafe) {
+            startActivity(mapIntent);
+        }
+    }
+
+    public void connect_api(View view) {
+        String stringUrl = "http://edgetracer.helinshiah.com";
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connManager.getActiveNetworkInfo();
+        if (networkInfo != null &&  networkInfo.isConnected()) {
+            new DownloadWebPageTask().execute(stringUrl);
+        }
+    }
+
+    private class DownloadWebPageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String...urls) {
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve";
+            }
+        }
+
+        private String downloadUrl(String myUrl) throws IOException {
+            InputStream is = null;
+            int len = 500;
+
+            try {
+                URL url = new URL(myUrl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                conn.connect();
+
+                int response = conn.getResponseCode();
+                is = conn.getInputStream();
+                DataInputStream dis = new DataInputStream(new BufferedInputStream(is));
+                String s;
+                while ((s = dis.readLine()) != null) {
+                    Log.d(DEBUG_TAG, s);
+                }
+//                Object contentResponse = conn.getContent();
+                Log.d(DEBUG_TAG, "The response is ... " + response);
+//                Log.d(DEBUG_TAG, "The content is ... " + contentResponse);
+                is = conn.getInputStream();
+
+                String contentAsString = readIt(is, len);
+                return contentAsString;
+            } finally {
+                if (is != null) {
+                    is.close();
+                }
+            }
+        }
+
+        public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+            Reader reader = null;
+            reader = new InputStreamReader(stream, "UTF-8");
+            char[] buffer = new char[len];
+            reader.read(buffer);
+            return new String(buffer);
+        }
     }
 }
